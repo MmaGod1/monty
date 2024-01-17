7ba83a7dccb4bcedcdb9d6bc008f67b636bb2419
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-17 09:36:55 -0800

    push and pall

--- pall.c
+++ pall.c
@@ -4,14 +4,16 @@
  * @stack: double pointer to the head of the stack
  * @line_number: line number in the file
  */
-void pall(stack_t **stack, unsigned int line_number) {
-    /* Implement pall opcode */
-    (void)line_number;
-  /* Unused parameter */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	/* Implement pall opcode */
+	(void)line_number;
+	/* Unused parameter */
+	stack_t *current = *stack;
 
-    stack_t *current = *stack;
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
--- push.c
+++ push.c
@@ -7,38 +7,43 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-int value;
-char *argument_str;
-stack_t *new_node;
-    /* Implementation of push opcode */
-    /* Check for usage error */
-    if (*stack == NULL)
-    {
-    fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	int value;
+	char *argument_str;
+	stack_t *new_node;
+	/* Implement push opcode */
+	/* Check for usage error */
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Get the integer argument */
-    argument_str = strtok(NULL, " \n");
-    if (!argument_str || !isdigit(*argument_str)) {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	/* Get the integer argument */
+	argument_str = strtok(NULL, " \n");
 
-    /* Convert argument to integer and push onto the stack */
-    value = atoi(argument_str);
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL) {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	if (!argument_str || !isdigit(*argument_str))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    if (*stack != NULL) {
-        (*stack)->prev = new_node;
-    }
+	/* Convert argument to integer and push onto the stack */
+	value = atoi(argument_str);
+	new_node = malloc(sizeof(stack_t));
 
-    *stack = new_node;
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
 }
