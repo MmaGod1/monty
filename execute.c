f4d0f69535a849585b92b3ef4b09303a901c3601
root <gmmaduchukwu@gmail.com>
2025-09-24 08:31:01 -0700

    Update function

--- execute.c
+++ execute.c
@@ -5,28 +5,40 @@
  * @stack: Pointer to the top of the stack.
  * @line: Line containing the Monty bytecode instruction
  * @line_number: Current line number in the Monty bytecode file
- * Return: void.
+ * Return: void
  */
 void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 {
 	char *opcode;
 	char *arg;
-	int value;
+	int i = 0, value;
 
-	/* Get the first token (the opcode) */
 	opcode = strtok(line, " \t\n");
 	if (opcode == NULL || opcode[0] == '#')
 		return;
 
-	/* Handle push (needs an argument) */
 	if (strcmp(opcode, "push") == 0)
 	{
 		arg = strtok(NULL, " \t\n");
-		if (arg == NULL || (!isdigit(arg[0]) && !(arg[0] == '-' && isdigit(arg[1]))))
+		if (arg == NULL)
 		{
 			fprintf(stderr, "L%u: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
+
+		/* Validate integer (allow negative numbers) */
+		if (arg[0] == '-')
+			i++;
+
+		for (; arg[i] != '\0'; i++)
+		{
+			if (!isdigit(arg[i]))
+			{
+				fprintf(stderr, "L%u: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+		}
+
 		value = atoi(arg);
 		push(stack, value, line_number);
 	}
--- push.c
+++ push.c
@@ -1,46 +1,24 @@
 #include "monty.h"
-
-/**
+/*
  * push - pushes an element onto the stack
  * @stack: double pointer to the head of the stack
- * @line_number: line number in the file
+ * @value: integer value to push
+ * @line_number: line number in the file (for error messages)
  *
  * Return: void
  */
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, int value, unsigned int line_number)
 {
-	char *arg = strtok(NULL, " \n\t");
-	stack_t *new_node;
-	int i = 0, value;
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-	/* check if argument exists */
-	if (arg == NULL)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+	(void)line_number;
 
-	/* validate integer (handle negative numbers too) */
-	if (arg[0] == '-')
-		i++;
-
-	for (; arg[i] != '\0'; i++)
-	{
-		if (!isdigit(arg[i]))
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	value = atoi(arg);
-
-	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+
 	new_node->n = value;
 	new_node->prev = NULL;
 	new_node->next = *stack;
