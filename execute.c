88179db68f3008c30032fded1d1af49ca29e8c06
root <gmmaduchukwu@gmail.com>
2025-09-24 08:22:20 -0700

    Update execute function

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * execute_instruction - executes monty bytecode instructions
  * @stack: Pointer to the top of the stack.
@@ -6,36 +7,44 @@
  * @line_number: Current line number in the Monty bytecode file
  * Return: void.
  */
-
 void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 {
-	char opcode[100];
-	int arg;
+	char *opcode;
+	char *arg;
+	int value;
 
-	/* Parse the instruction from the line */
-	if (sscanf(line, " %99s %d", opcode, &arg) == 2)
+	/* Get the first token (the opcode) */
+	opcode = strtok(line, " \t\n");
+	if (opcode == NULL || opcode[0] == '#')
+		return;
+
+	/* Handle push (needs an argument) */
+	if (strcmp(opcode, "push") == 0)
 	{
-		if (strcmp(opcode, "push") == 0)
+		arg = strtok(NULL, " \t\n");
+		if (arg == NULL || (!isdigit(arg[0]) && !(arg[0] == '-' && isdigit(arg[1]))))
 		{
-			push(stack, line_number);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(stack, line_number);
-		}
-		else if (strcmp(opcode, "pint") == 0)
-		{
-			pint(stack, line_number);
-		}
-		else
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
+		value = atoi(arg);
+		push(stack, value, line_number);
 	}
-	else if (strcmp(line, "nop\n") != 0)
+	else if (strcmp(opcode, "pall") == 0)
 	{
-		fprintf(stderr, "L%d: invalid instruction format\n", line_number);
+		pall(stack, line_number);
+	}
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		pint(stack, line_number);
+	}
+	else if (strcmp(opcode, "nop") == 0)
+	{
+		/* do nothing */
+	}
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
 		exit(EXIT_FAILURE);
 	}
 }
