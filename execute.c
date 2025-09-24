ba61bdc7527fa184b3acd0b241954880edc62427
root <gmmaduchukwu@gmail.com>
2025-09-24 08:53:47 -0700

    Update

--- execute.c
+++ execute.c
@@ -1,15 +1,16 @@
 #include "monty.h"
 
 /**
- * handle_push - handles the push opcode
- * @stack: pointer to the top of the stack
- * @arg: argument string (expected integer)
+ * handle_push - validates and executes push
+ * @stack: pointer to top of stack
  * @line_number: line number in file
  */
-void handle_push(stack_t **stack, char *arg, unsigned int line_number)
+void handle_push(stack_t **stack, unsigned int line_number)
 {
+	char *arg;
 	int i = 0, value;
 
+	arg = strtok(NULL, " \t\n");
 	if (arg == NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
@@ -18,7 +19,6 @@ void handle_push(stack_t **stack, char *arg, unsigned int line_number)
 
 	if (arg[0] == '-')
 		i++;
-
 	for (; arg[i] != '\0'; i++)
 	{
 		if (!isdigit(arg[i]))
@@ -33,28 +33,27 @@ void handle_push(stack_t **stack, char *arg, unsigned int line_number)
 }
 
 /**
- * execute_instruction - executes monty bytecode instructions
- * @stack: Pointer to the top of the stack.
- * @line: Line containing the Monty bytecode instruction
- * @line_number: Current line number in the Monty bytecode file
- * Return: void
+ * execute_instruction - executes Monty bytecode instructions
+ * @stack: pointer to top of stack
+ * @line: current line
+ * @line_number: line number in file
  */
 void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 {
-	char *opcode, *arg;
+	char *opcode;
 
 	opcode = strtok(line, " \t\n");
 	if (opcode == NULL || opcode[0] == '#')
 		return;
 
 	if (strcmp(opcode, "push") == 0)
-		handle_push(stack, strtok(NULL, " \t\n"), line_number);
+		handle_push(stack, line_number);
 	else if (strcmp(opcode, "pall") == 0)
 		pall(stack, line_number);
 	else if (strcmp(opcode, "pint") == 0)
 		pint(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
-		; /* do nothing */
+		;
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
