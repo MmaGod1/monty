ea85e594a4f63fdc6e41068918a46c7528cbe051
root <gmmaduchukwu@gmail.com>
2025-09-24 08:58:04 -0700

    Update pop function

--- pop.c
+++ pop.c
@@ -1,31 +1,24 @@
 #include "monty.h"
 
 /**
- * pop - Removes the top element of the stack.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number in the Monty byte code file.
+ * pop - removes the top element of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number in file
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
-
-	if ((*stack)->next != NULL)
-	{
-		*stack = (*stack)->next;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
 		(*stack)->prev = NULL;
-	}
-	else
-	{
-		*stack = NULL;
-	}
 
 	free(temp);
 }
