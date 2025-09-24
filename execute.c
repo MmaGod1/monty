23dc96f6c0c506612d7484aba67f722c16c1ac3d
root <gmmaduchukwu@gmail.com>
2025-09-24 09:11:14 -0700

    Add swap instruction

--- execute.c
+++ execute.c
@@ -54,6 +54,8 @@ void execute_instruction(stack_t **stack, char *line, unsigned int line_number)
 		pint(stack, line_number);
 	else if (strcmp(opcode, "pop") == 0)
 		pop(stack, line_number);
+	else if (strcmp(opcode, "swap") == 0)
+		swap(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
 		;
 	else
